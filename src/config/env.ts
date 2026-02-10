9f6070c435a5b52c6d2623baa03f52d60c986e88
vong <vongsouvan09@gmail.com>
2026-02-10 11:22:30 +0700

    feat: Add environment variable configuration and loading using dotenv.

--- src/config/env.ts
+++ src/config/env.ts
@@ -19,5 +19,5 @@ export const env = {
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
   JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
-  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
 } as const;
